97bf06480edfbe57882450c1e05ba0d6d185f3d3
Mizume25 <gabrielnivicela649@gmail.com>
2026-03-11 11:05:28 +0100

    ajustes

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -3,12 +3,17 @@ import AppLogoIcon from './app-logo-icon';
 export default function AppLogo() {
     return (
         <>
-            <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
-                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
+            {/* Este div ya es el cuadrado negro redondeado */}
+            <div className="flex size-8 items-center justify-center dark:bg-white text-white dark:text-black">
+                <AppLogoIcon className="size-5" />
             </div>
-            <div className="ml-1 grid flex-1 text-left text-sm">
-                <span className="mb-0.5 truncate leading-tight font-semibold">
-                    Laravel Starter Kit
+            
+            <div className="ml-3 grid flex-1 text-left text-sm">
+                <span className="truncate leading-tight font-bold text-base">
+                    Lumina
+                </span>
+                <span className="truncate text-xs text-muted-foreground">
+                    Instituto Superior
                 </span>
             </div>
         </>
